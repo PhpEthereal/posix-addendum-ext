8832aff9d05fe335429fe960f4c285161aaede10
Paul Water <p.r.water@setbased.nl>
2020-03-26 13:55:34 +0100

    Untabified source files.

--- posix_addendum.c
+++ posix_addendum.c
@@ -13,76 +13,76 @@
 /* For compatibility with older PHP versions */
 #ifndef ZEND_PARSE_PARAMETERS_NONE
 #define ZEND_PARSE_PARAMETERS_NONE() \
-				ZEND_PARSE_PARAMETERS_START(0, 0) \
-				ZEND_PARSE_PARAMETERS_END()
+        ZEND_PARSE_PARAMETERS_START(0, 0) \
+        ZEND_PARSE_PARAMETERS_END()
 #endif
 
 #if HAVE_SOCKETS || defined(COMPILE_DL_SOCKETS)
-#include "ext/sockets/php_sockets.h"
-#define POSIX_ADDENDUM_USE_SOCKETS
+# include "ext/sockets/php_sockets.h"
+# define POSIX_ADDENDUM_USE_SOCKETS
 #endif
 
 /* {{{ php_posix_addendum_zval_to_fd
  * Get numeric file descriptor from PHP stream or Socket resource */
 static php_socket_t php_posix_addendum_zval_to_fd(zval *pzfd)
 {
-	php_socket_t file_desc = -1;
-	php_stream *stream;
+  php_socket_t file_desc = -1;
+  php_stream *stream;
 #ifdef POSIX_ADDENDUM_USE_SOCKETS
-	php_socket *php_sock;
+  php_socket *php_sock;
 #endif
-	if (Z_TYPE_P(pzfd) == IS_RESOURCE)
-	{
-		/* PHP stream or PHP socket resource  */
-		if ((stream = (php_stream *) zend_fetch_resource_ex(pzfd, NULL, php_file_le_stream())))
-		{
-			/* PHP stream */
-			if (php_stream_cast(stream,
-													PHP_STREAM_AS_FD | PHP_STREAM_CAST_INTERNAL,
-													(void*)&file_desc, 1) != SUCCESS || file_desc < 0)
-			{
-				return -1;
-			}
-		}
-		else
-		{
-			/* PHP socket resource */
+  if (Z_TYPE_P(pzfd) == IS_RESOURCE)
+  {
+    /* PHP stream or PHP socket resource  */
+    if ((stream = (php_stream *) zend_fetch_resource_ex(pzfd, NULL, php_file_le_stream())))
+    {
+      /* PHP stream */
+      if (php_stream_cast(stream,
+                          PHP_STREAM_AS_FD | PHP_STREAM_CAST_INTERNAL,
+                          (void*)&file_desc, 1) != SUCCESS || file_desc < 0)
+      {
+        return -1;
+      }
+    }
+    else
+    {
+      /* PHP socket resource */
 #ifdef POSIX_ADDENDUM_USE_SOCKETS
-			if ((php_sock = zend_fetch_resource_ex(pzfd, NULL, php_sockets_le_socket())))
-			{
-				return php_sock->bsd_socket;
-			}
+      if ((php_sock = zend_fetch_resource_ex(pzfd, NULL, php_sockets_le_socket())))
+      {
+        return php_sock->bsd_socket;
+      }
 
-			php_error_docref(NULL, E_WARNING, "either valid PHP stream or valid PHP socket resource expected");
+      php_error_docref(NULL, E_WARNING, "either valid PHP stream or valid PHP socket resource expected");
 
-			return -1;
+      return -1;
 #else
-			php_error_docref(NULL, E_WARNING, "valid PHP stream resource expected");
+      php_error_docref(NULL, E_WARNING, "valid PHP stream resource expected");
 
-			return -1;
+      return -1;
 #endif
-		}
-	}
-	else if (Z_TYPE_P(pzfd) == IS_LONG)
-	{
-		/* Numeric fd */
-		file_desc = Z_LVAL_P(pzfd);
-		if (file_desc < 0)
-		{
-			php_error_docref(NULL, E_WARNING, "invalid file descriptor passed");
+    }
+  }
+  else if (Z_TYPE_P(pzfd) == IS_LONG)
+  {
+    /* Numeric fd */
+    file_desc = Z_LVAL_P(pzfd);
+    if (file_desc < 0)
+    {
+      php_error_docref(NULL, E_WARNING, "invalid file descriptor passed");
 
-			return -1;
-		}
-	}
-	else
-	{
-		/* Invalid fd */
-		php_error_docref(NULL, E_WARNING, "invalid file descriptor passed");
+      return -1;
+    }
+  }
+  else
+  {
+    /* Invalid fd */
+    php_error_docref(NULL, E_WARNING, "invalid file descriptor passed");
 
-		return -1;
-	}
+    return -1;
+  }
 
-	return file_desc;
+  return file_desc;
 }
 /* }}} */
 
@@ -91,14 +91,14 @@ static php_socket_t php_posix_addendum_zval_to_fd(zval *pzfd)
  * Does not return. */
 PHP_FUNCTION(posix_addendum_immediate_exit)
 {
-	zend_long status;
+  zend_long status;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS(), "l",  &status) == FAILURE)
-	{
-		return;
-	}
+  if (zend_parse_parameters(ZEND_NUM_ARGS(), "l",  &status) == FAILURE)
+  {
+    return;
+  }
 
-	_exit(status);
+  _exit(status);
 }
 /* }}} */
 
@@ -108,28 +108,28 @@ PHP_FUNCTION(posix_addendum_immediate_exit)
  * Returns TRUE on success, otherwise FALSE. */
 PHP_FUNCTION(posix_addendum_dup2)
 {
-	zval *zfd, *zfd2;
-	int fd, fd2, err;
+  zval *zfd, *zfd2;
+  int fd, fd2, err;
 
-	if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &zfd, &zfd2) == FAILURE)
-	{
-		return;
-	}
+  if (zend_parse_parameters(ZEND_NUM_ARGS(), "zz", &zfd, &zfd2) == FAILURE)
+  {
+    return;
+  }
 
-	fd = php_posix_addendum_zval_to_fd(zfd);
-	fd2 = php_posix_addendum_zval_to_fd(zfd2);
-	if (fd < 0 || fd2 < 0)
-	{
-		RETURN_FALSE;
-	}
+  fd = php_posix_addendum_zval_to_fd(zfd);
+  fd2 = php_posix_addendum_zval_to_fd(zfd2);
+  if (fd < 0 || fd2 < 0)
+  {
+    RETURN_FALSE;
+  }
 
-	err = dup2(fd, fd2);
-	if (err == -1)
-	{
-		RETURN_FALSE;
-	}
+  err = dup2(fd, fd2);
+  if (err == -1)
+  {
+    RETURN_FALSE;
+  }
 
-	RETURN_TRUE;
+  RETURN_TRUE;
 }
 /* }}} */
 
@@ -138,10 +138,10 @@ PHP_FUNCTION(posix_addendum_dup2)
 PHP_RINIT_FUNCTION(posix_addendum)
 {
 #if defined(ZTS) && defined(COMPILE_DL_POSIX_ADDENDUM)
-	ZEND_TSRMLS_CACHE_UPDATE();
+  ZEND_TSRMLS_CACHE_UPDATE();
 #endif
 
-	return SUCCESS;
+  return SUCCESS;
 }
 /* }}} */
 
@@ -149,33 +149,33 @@ PHP_RINIT_FUNCTION(posix_addendum)
  */
 PHP_MINFO_FUNCTION(posix_addendum)
 {
-	php_info_print_table_start();
-	php_info_print_table_header(2, "posix_addendum support", "enabled");
-	php_info_print_table_end();
+  php_info_print_table_start();
+  php_info_print_table_header(2, "posix_addendum support", "enabled");
+  php_info_print_table_end();
 }
 /* }}} */
 
 static const zend_function_entry posix_addendum_functions[] =
 {
-	PHP_FE(posix_addendum_immediate_exit, arginfo_posix_addendum_immediate_exit)
-	PHP_FE(posix_addendum_dup2, arginfo_posix_addendum_dup2)
-	PHP_FE_END
+  PHP_FE(posix_addendum_immediate_exit, arginfo_posix_addendum_immediate_exit)
+  PHP_FE(posix_addendum_dup2, arginfo_posix_addendum_dup2)
+  PHP_FE_END
 };
 
 /* {{{ posix_addendum_module_entry
  */
 zend_module_entry posix_addendum_module_entry =
 {
-	STANDARD_MODULE_HEADER,
-	PHP_POSIX_ADDENDUM_EXTNAME,
-	posix_addendum_functions,
-	NULL,
-	NULL,
-	PHP_RINIT(posix_addendum),
-	NULL,
-	PHP_MINFO(posix_addendum),
-	PHP_POSIX_ADDENDUM_VERSION,
-	STANDARD_MODULE_PROPERTIES
+  STANDARD_MODULE_HEADER,
+  PHP_POSIX_ADDENDUM_EXTNAME,
+  posix_addendum_functions,
+  NULL,
+  NULL,
+  PHP_RINIT(posix_addendum),
+  NULL,
+  PHP_MINFO(posix_addendum),
+  PHP_POSIX_ADDENDUM_VERSION,
+  STANDARD_MODULE_PROPERTIES
 };
 /* }}} */
 
--- posix_addendum_arginfo.h
+++ posix_addendum_arginfo.h
@@ -1,10 +1,10 @@
 /* This is a generated file, edit the .stub.php file instead. */
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_posix_addendum_immediate_exit, 0, 1, IS_VOID, 0)
-	ZEND_ARG_TYPE_INFO(0, status, IS_LONG, 0)
+  ZEND_ARG_TYPE_INFO(0, status, IS_LONG, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_posix_addendum_dup2, 0, 2, _IS_BOOL, 0)
-	ZEND_ARG_INFO(0, fd)
-	ZEND_ARG_INFO(0, fd2)
+  ZEND_ARG_INFO(0, fd)
+  ZEND_ARG_INFO(0, fd2)
 ZEND_END_ARG_INFO()
